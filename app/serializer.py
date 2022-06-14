4f2152ea84e4d9ab6dc584089093b540ea0c4bb8
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-14 21:17:01 +0300

    ft: Post serializer class created

--- app/serializer.py
+++ app/serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Profile,Post
 
-
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']

56668ee6815f57c959f5ea33cfb38f748f12ed42
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-15 14:10:24 +0300

    ft: Upload project form created

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Profile
+from .models import Profile,Post
 
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'contact', 'bio']
+
+class UploadProjectForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'image', 'url', 'technologies','description']
